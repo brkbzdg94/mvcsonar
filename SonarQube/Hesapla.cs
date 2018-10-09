d1cbc842553c7c18b7d70ce050b297eba40c054a
Burak <cixnx@hotmail.com>
2018-10-09 11:12:26 +0300

    duplication

--- SonarQube/Hesapla.cs
+++ SonarQube/Hesapla.cs
@@ -34,17 +34,17 @@ namespace SonarQube
             Console.Write("Burak");
         }
 
-        public void sonar2()
-        {
-            int target = -5;
-            int num = 3;
-            target = -num;  // Noncompliant; target = -3. Is that really what's meant?
-            target = +num; // Noncompliant; target = 3
-            Console.Write("Burak");
-            Console.Write("Burak");
-            Console.Write("Burak");
-            Console.Write("Burak");
+        //public void sonar2()
+        //{
+        //    int target = -5;
+        //    int num = 3;
+        //    target = -num;  // Noncompliant; target = -3. Is that really what's meant?
+        //    target = +num; // Noncompliant; target = 3
+        //    Console.Write("Burak");
+        //    Console.Write("Burak");
+        //    Console.Write("Burak");
+        //    Console.Write("Burak");
 
-        }
+        //}
     }
 }
---
+++ SonarQube/Hesapla2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SonarQube
+{
+    public class Hesapla2
+    {
+        public void sonar2()
+        {
+            int target = -5;
+            int num = 3;
+            target = -num;  // Noncompliant; target = -3. Is that really what's meant?
+            target = +num; // Noncompliant; target = 3
+            Console.Write("Burak");
+            Console.Write("Burak");
+            Console.Write("Burak");
+            Console.Write("Burak");
+        }
+    }
+}
